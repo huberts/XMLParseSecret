fee55e236545bfdc067709a6a020a498497b2c22
Hubert Świetlicki <hswietlicki@systherm-info.pl>
2022-04-13 11:11:42 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+
+    [XmlRoot(ElementName = "Note")]
+    public class Note
+    {
+        [XmlElement(ElementName = "From")]
+        public string From { get; set; }
+        [XmlElement(ElementName = "To")]
+        public string To { get; set; }
+        [XmlElement(ElementName = "Encoded")]
+        public string Encoded { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Secret")]
+    public class Secret
+    {
+        [XmlElement(ElementName = "Key")]
+        public string Key { get; set; }
+        [XmlElement(ElementName = "Value")]
+        public string Value { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var noteSerializer = new XmlSerializer(typeof(Note));
+            Note n;
+            using (Stream reader = new FileStream(Path.Combine(Environment.CurrentDirectory, "../../..", "input.xml"), FileMode.Open))
+            {
+                n = (Note)noteSerializer.Deserialize(reader);
+            }
+
+            var secretSerializer = new XmlSerializer(typeof(Secret));
+            Secret s = (Secret)secretSerializer.Deserialize(new MemoryStream(Encoding.UTF8.GetBytes(n.Encoded.Trim())));
+
+        }
+    }
+}
